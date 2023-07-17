741b4d394bab2d03047fc380503aea088ddaa86a
Débora Inocêncio <30512134+MQJogos@users.noreply.github.com>
2023-07-16 22:24:45 -0300

    Update login.js

--- login.js
+++ login.js
@@ -1,5 +1,9 @@
 class Login{
-
+   static logado=false;
+   static matlogado=null;
+   static nomelogado=null;
+   static acessologado=null;
+   static endpoint="";
    static login=()=>{
       
    }
